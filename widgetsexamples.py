6a9c9933407ad3b2394f0d023e925b5398263be5
Taner Süzen <taner.suzen95@gmail.com>
2023-11-29 23:56:03 +0300

    revision1

--- widgetsexamples.py
+++ widgetsexamples.py
@@ -3,12 +3,11 @@ from tkinter import *
 window = Tk()
 window.title("Tkinter Python")
 window.minsize(width=600,height=600)
-window.config(padx=60,pady=150)
 
 my_label = Label()
 my_label.config(bg="black")
 my_label.config(fg="white")
-my_label.config(padx=100, pady=100)
+my_label.config(padx=30, pady=30)
 my_label.pack()
 
 #button
@@ -25,7 +24,7 @@ my_entry = Entry(width=20)
 my_entry.pack()
 
 #multiline, text
-my_text = Text(width=30, height=10)
+my_text = Text(width=30, height=5)
 my_text.pack()
 
 #scale
@@ -34,5 +33,38 @@ def scale_selected(value):
 my_scale = Scale(from_=0,to=50,command=scale_selected)
 my_scale.pack()
 
+#spinbox
+def spinbox_selected():
+    print(my_spinbox.get())
+my_spinbox = Spinbox(from_=0,to=50,command=spinbox_selected)
+my_spinbox.pack()
+
+#checkbutton
+def checkbutton_selected():
+    print(check_state.get())
+
+check_state = IntVar()
+my_checkbutton = Checkbutton(text="check",variable=check_state,command=checkbutton_selected)
+my_checkbutton.pack()
+
+#radio button
+def radio_selected():
+    print(radio_checked_state.get())
+radio_checked_state = IntVar()
+my_radiobutton = Radiobutton(text="1. option",value=10,variable=radio_checked_state,command=radio_selected)
+my_radiobutton_2 = Radiobutton(text="2. option",value=20,variable=radio_checked_state,command=radio_selected)
+my_radiobutton.pack()
+my_radiobutton_2.pack()
+
+#Listbox
+def listbox_selected(event):
+    print(my_listbox.get(my_listbox.curselection()))
+
+my_listbox = Listbox()
+name_list = ["Taner","abc","yntnyn","safweeqw","qsdfqas"]
+for i in range(len(name_list)):
+    my_listbox.insert(i,name_list[i])
+my_listbox.bind('   <<ListboxSelect>>',listbox_selected)
+my_listbox.pack()
 
 window.mainloop()
